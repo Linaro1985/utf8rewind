43309823b5178fbb4a2b83d69e16855ac06d8329
Anne <knight666@gmail.com>
2014-06-19 15:26:48 +0200

    Added detection for character size of wide string encoding.

---
+++ documentation/html/utf8rewind_8h.js
@@ -0,0 +1,22 @@
+var utf8rewind_8h =
+[
+    [ "UTF8_ERR_INVALID_CHARACTER", "utf8rewind_8h.html#aa3a118249a9ae08e152079f4989ca588", null ],
+    [ "UTF8_ERR_INVALID_DATA", "utf8rewind_8h.html#aabf4ed55c67112f79abe7275ee2ecc9b", null ],
+    [ "UTF8_ERR_NOT_ENOUGH_SPACE", "utf8rewind_8h.html#a2c80a4201b65d282db98489204f6d5d8", null ],
+    [ "UTF8_ERR_OUT_OF_RANGE", "utf8rewind_8h.html#a26c37b1e55b873bc49642319dc3d19e5", null ],
+    [ "UTF8_ERR_UNHANDLED_SURROGATE_PAIR", "utf8rewind_8h.html#aa0c5762ac11727331a3538f8e0a56104", null ],
+    [ "UTF8_ERR_UNMATCHED_HIGH_SURROGATE_PAIR", "utf8rewind_8h.html#a0661d820dd006e49adfbdf0c7a9dec14", null ],
+    [ "UTF8_ERR_UNMATCHED_LOW_SURROGATE_PAIR", "utf8rewind_8h.html#a892fe61a47f8258d88e292553a459a1c", null ],
+    [ "ucs2_t", "utf8rewind_8h.html#aaed921fc64460bb8f0039ba06c5abd17", null ],
+    [ "unicode_t", "utf8rewind_8h.html#a5fc61747372b4165739ae5a52384468f", null ],
+    [ "utf16_t", "utf8rewind_8h.html#a1e9e182e040e6f408cfb7188eaafb48a", null ],
+    [ "utf8charlen", "utf8rewind_8h.html#a4c456e1c3d8637bdf3f999aa0377b2f2", null ],
+    [ "utf8charvalid", "utf8rewind_8h.html#a9d19522cd09e2dff6fca8db6a3a5cacf", null ],
+    [ "utf8convertucs2", "utf8rewind_8h.html#ac08ff5b9be438e67b8e0af6a4c56543f", null ],
+    [ "utf8decode", "utf8rewind_8h.html#a4d0c2482832b035a189e96a0d3d80e4c", null ],
+    [ "utf8encode", "utf8rewind_8h.html#ab936e1848e79cdff95c834a294a7539c", null ],
+    [ "utf8len", "utf8rewind_8h.html#aa6a3ebdecf49591cff736ccf054c048e", null ],
+    [ "utf8seek", "utf8rewind_8h.html#a13d55ea3402d6d91cfb965f3605ec3eb", null ],
+    [ "utf8towc", "utf8rewind_8h.html#afe10f10be61afc896374cac318c8494d", null ],
+    [ "wctoutf8", "utf8rewind_8h.html#ac472a3dfa44e41bda08513860b61317d", null ]
+];
---
+++ utf8rewind.gyp
@@ -0,0 +1,53 @@
+{
+	'includes': [
+		'build/common.gypi',
+	],
+	'targets': [
+		{
+			'target_name': 'utf8rewind',
+			'type': 'static_library',
+			'include_dirs': [
+				'include/utf8rewind',
+			],
+			'sources': [
+				'include/utf8rewind/utf8rewind.h',
+				'source/utf8rewind.c',
+			],
+			'direct_dependent_settings': {
+				'include_dirs': [
+					'include/utf8rewind',
+				],
+			},
+			'conditions': [
+				['OS=="win"', {
+					'msvs_settings': {
+						'VCCLCompilerTool': {
+							'CompileAs': 1, # Compile as C
+						},
+					},
+				}],
+			],
+		},
+		{
+			'target_name': 'tests-rewind',
+			'type': 'executable',
+			'dependencies': [
+				'dependencies.gyp:gtest',
+				'utf8rewind',
+			],
+			'sources': [
+				'source/tests/suite-charlen.cpp',
+				'source/tests/suite-charvalid.cpp',
+				'source/tests/suite-convertucs2.cpp',
+				'source/tests/suite-decode.cpp',
+				'source/tests/suite-encode.cpp',
+				'source/tests/suite-len.cpp',
+				'source/tests/suite-seek.cpp',
+				'source/tests/suite-toutf8.cpp',
+				'source/tests/suite-towc.cpp',
+				'source/tests/tests-base.hpp',
+				'source/tests/tests-main.cpp',
+			],
+		},
+	],
+}
