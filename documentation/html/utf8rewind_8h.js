0b66306c7afeac6b487bc8b0391afa0862da4049
Anne <knight666@gmail.com>
2014-06-30 09:15:46 +0200

    Fixed makefile generation on Linux.

---
+++ documentation/html/utf8rewind_8h.js
@@ -0,0 +1,22 @@
+var utf8rewind_8h =
+[
+    [ "UTF8_ERR_INVALID_CHARACTER", "utf8rewind_8h.html#aa3a118249a9ae08e152079f4989ca588", null ],
+    [ "UTF8_ERR_INVALID_DATA", "utf8rewind_8h.html#aabf4ed55c67112f79abe7275ee2ecc9b", null ],
+    [ "UTF8_ERR_NOT_ENOUGH_SPACE", "utf8rewind_8h.html#a2c80a4201b65d282db98489204f6d5d8", null ],
+    [ "UTF8_ERR_OUT_OF_RANGE", "utf8rewind_8h.html#a26c37b1e55b873bc49642319dc3d19e5", null ],
+    [ "UTF8_ERR_UNHANDLED_SURROGATE_PAIR", "utf8rewind_8h.html#aa0c5762ac11727331a3538f8e0a56104", null ],
+    [ "UTF8_ERR_UNMATCHED_HIGH_SURROGATE_PAIR", "utf8rewind_8h.html#a0661d820dd006e49adfbdf0c7a9dec14", null ],
+    [ "UTF8_ERR_UNMATCHED_LOW_SURROGATE_PAIR", "utf8rewind_8h.html#a892fe61a47f8258d88e292553a459a1c", null ],
+    [ "ucs2_t", "utf8rewind_8h.html#aaed921fc64460bb8f0039ba06c5abd17", null ],
+    [ "unicode_t", "utf8rewind_8h.html#a5fc61747372b4165739ae5a52384468f", null ],
+    [ "utf16_t", "utf8rewind_8h.html#a1e9e182e040e6f408cfb7188eaafb48a", null ],
+    [ "utf8charlen", "utf8rewind_8h.html#a46a4b4215a32429f0726dd0dffb6e718", null ],
+    [ "utf8charvalid", "utf8rewind_8h.html#a548f6f67979ffd8576a6f45b2a3455e1", null ],
+    [ "utf8convertucs2", "utf8rewind_8h.html#ac08ff5b9be438e67b8e0af6a4c56543f", null ],
+    [ "utf8decode", "utf8rewind_8h.html#a33dcf979fa8ab17d40c0b7c7645a5dfa", null ],
+    [ "utf8encode", "utf8rewind_8h.html#ac9b9b9d684c8bae0ad20c26d2d946986", null ],
+    [ "utf8len", "utf8rewind_8h.html#aa6a3ebdecf49591cff736ccf054c048e", null ],
+    [ "utf8seek", "utf8rewind_8h.html#a13d55ea3402d6d91cfb965f3605ec3eb", null ],
+    [ "utf8towc", "utf8rewind_8h.html#a17370cc081b5bd688c6d95bf9b09f72f", null ],
+    [ "wctoutf8", "utf8rewind_8h.html#ae68f9a9de59685c36fb0098f373b7f8b", null ]
+];
