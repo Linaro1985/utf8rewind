9a2ec4dbd47dcbf1f2ff3546fe37103f53859ecb
Anne <knight666@gmail.com>
2015-01-05 17:19:17 +0100

    Merge with case-mapping

---
+++ source/tests/helpers-strings.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "tests-base.hpp"
+
+#include "utf8rewind.h"
+
+#define EXPECT_UTF8EQ(_expected, _actual) EXPECT_PRED_FORMAT2(::helpers::CompareUtf8Strings, _expected, _actual);
+
+namespace helpers {
+
+	std::string identifiable(unicode_t codepoint);
+	std::string identifiable(const std::string& text);
+
+	std::string utf8(unicode_t codepoint);
+	std::string utf8(const std::vector<unicode_t>& codepoints);
+
+	std::string printable(const std::string& text);
+	std::string printable(unicode_t codepoint);
+	std::string printable(unicode_t* codepoints, size_t codepointsSize);
+
+	::testing::AssertionResult CompareUtf8Strings(
+		const char* expressionExpected, const char* expressionActual,
+		const char* textExpected, const char* textActual
+	);
+
+};
---
+++ source/tests/integration-internal-composition.cpp
@@ -0,0 +1,142 @@
+#include "tests-base.hpp"
+
+extern "C" {
+	#include "../normalization.h"
+}
+
+#include "helpers-strings.hpp"
+
+TEST(Composition, SingletonAngstrom)
+{
+	int32_t errors = 0;
+
+	// NFD
+
+	{
+		const char* d = finddecomposition(0x212B, UnicodeProperty_Normalization_Decompose, &errors);
+		ASSERT_NE(nullptr, d);
+		EXPECT_EQ(0, errors);
+
+		EXPECT_UTF8EQ("A\xCC\x8A", d);
+		EXPECT_STREQ("A\\u30A", helpers::identifiable(d).c_str());
+	}
+
+	// NFKD
+
+	{
+		const char* d = finddecomposition(0x212B, UnicodeProperty_Normalization_Compatibility_Decompose, &errors);
+		ASSERT_NE(nullptr, d);
+		EXPECT_EQ(0, errors);
+
+		EXPECT_UTF8EQ("A\xCC\x8A", d);
+		EXPECT_STREQ("A\\u30A", helpers::identifiable(d).c_str());
+	}
+}
+
+TEST(Composition, SingletonOhm)
+{
+	int32_t errors = 0;
+
+	// NFD
+
+	{
+		const char* d = finddecomposition(0x2126, UnicodeProperty_Normalization_Decompose, &errors);
+		ASSERT_NE(nullptr, d);
+		EXPECT_EQ(0, errors);
+
+		EXPECT_UTF8EQ("\xCE\xA9", d);
+		EXPECT_STREQ("\\u3A9", helpers::identifiable(d).c_str());
+	}
+
+	// NFKD
+
+	{
+		const char* d = finddecomposition(0x2126, UnicodeProperty_Normalization_Compatibility_Decompose, &errors);;
+		ASSERT_NE(nullptr, d);
+		EXPECT_EQ(0, errors);
+
+		EXPECT_UTF8EQ("\xCE\xA9", d);
+		EXPECT_STREQ("\\u3A9", helpers::identifiable(d).c_str());
+	}
+}
+
+TEST(Composition, CanonicalCompositeAWithRing)
+{
+	int32_t errors = 0;
+
+	// NFD
+
+	{
+		const char* d = finddecomposition(0xC5, UnicodeProperty_Normalization_Decompose, &errors);
+		ASSERT_NE(nullptr, d);
+		EXPECT_EQ(0, errors);
+
+		EXPECT_UTF8EQ("A\xCC\x8A", d);
+		EXPECT_STREQ("A\\u30A", helpers::identifiable(d).c_str());
+	}
+
+	// NFKD
+
+	{
+		const char* d = finddecomposition(0xC5, UnicodeProperty_Normalization_Compatibility_Decompose, &errors);
+		ASSERT_NE(nullptr, d);
+		EXPECT_EQ(0, errors);
+
+		EXPECT_UTF8EQ("A\xCC\x8A", d);
+		EXPECT_STREQ("A\\u30A", helpers::identifiable(d).c_str());
+	}
+}
+
+TEST(Composition, CanonicalCompositeOWithCircumflex)
+{
+	int32_t errors = 0;
+
+	// NFD
+
+	{
+		const char* d = finddecomposition(0xF4, UnicodeProperty_Normalization_Decompose, &errors);
+		ASSERT_NE(nullptr, d);
+		EXPECT_EQ(0, errors);
+
+		EXPECT_UTF8EQ("o\xCC\x82", d);
+		EXPECT_STREQ("o\\u302", helpers::identifiable(d).c_str());
+	}
+
+	// NFKD
+
+	{
+		const char* d = finddecomposition(0xF4, UnicodeProperty_Normalization_Compatibility_Decompose, &errors);
+		ASSERT_NE(nullptr, d);
+		EXPECT_EQ(0, errors);
+
+		EXPECT_UTF8EQ("o\xCC\x82", d);
+		EXPECT_STREQ("o\\u302", helpers::identifiable(d).c_str());
+	}
+}
+
+TEST(Composition, MultipleCombiningMarksSWithDots)
+{
+	int32_t errors = 0;
+
+	// NFD
+
+	{
+		const char* d = finddecomposition(0x1E69, UnicodeProperty_Normalization_Decompose, &errors);
+		ASSERT_NE(nullptr, d);
+		EXPECT_EQ(0, errors);
+
+		EXPECT_UTF8EQ("s\xCC\xA3\xCC\x87", d);
+		EXPECT_STREQ("s\\u323\\u307", helpers::identifiable(d).c_str());
+	}
+
+	// NFKD
+
+	{
+		const char* d = finddecomposition(0x1E69, UnicodeProperty_Normalization_Compatibility_Decompose, &errors);
+		ASSERT_NE(nullptr, d);
+		EXPECT_EQ(0, errors);
+
+		EXPECT_UTF8EQ("s\xCC\xA3\xCC\x87", d);
+		EXPECT_STREQ("s\\u323\\u307", helpers::identifiable(d).c_str());
+	}
+}
