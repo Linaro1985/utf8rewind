b9f3fa0080453d622b2ac806f9b25e198f010fc9
Quinten <quintenlansu@gmail.com>
2015-02-05 15:48:00 +0100

    Merge with normalization

---
+++ source/tests/helpers-normalization.cpp
@@ -0,0 +1,319 @@
+#include "helpers-normalization.hpp"
+
+#include "helpers-strings.hpp"
+
+extern "C" {
+	#include "../internal/database.h"
+};
+
+namespace helpers {
+
+	std::string nfc(unicode_t codepointLeft, unicode_t codepointRight)
+	{
+		std::string converted;
+		converted += utf8(codepointLeft);
+		converted += utf8(codepointRight);
+
+		return nfc(converted);
+	}
+
+	std::string nfc(const std::string& text)
+	{
+		int32_t errors = 0;
+
+		size_t length = utf8normalize(text.c_str(), text.size(), nullptr, 0, UTF8_NORMALIZE_COMPOSE, &errors);
+		if (length == 0 ||
+			errors != 0)
+		{
+			return "";
+		}
+
+		char* buffer = new char[length + 1];
+		utf8normalize(text.c_str(), text.size(), buffer, length, UTF8_NORMALIZE_COMPOSE, &errors);
+		buffer[length] = 0;
+
+		std::string converted = buffer;
+
+		delete [] buffer;
+
+		return converted;
+	}
+
+	std::string nfd(unicode_t codepoint)
+	{
+		return nfd(utf8(codepoint));
+	}
+
+	std::string nfd(const std::string& text)
+	{
+		int32_t errors = 0;
+
+		size_t length = utf8normalize(text.c_str(), text.size(), nullptr, 0, UTF8_NORMALIZE_DECOMPOSE, &errors);
+		if (length == 0 ||
+			errors != 0)
+		{
+			return "";
+		}
+
+		char* buffer = new char[length + 1];
+		utf8normalize(text.c_str(), text.size(), buffer, length, UTF8_NORMALIZE_DECOMPOSE, &errors);
+		buffer[length] = 0;
+
+		std::string converted = buffer;
+
+		delete [] buffer;
+
+		return converted;
+	}
+
+	std::string nfkc(unicode_t codepointLeft, unicode_t codepointRight)
+	{
+		std::string converted;
+		converted += utf8(codepointLeft);
+		converted += utf8(codepointRight);
+
+		return nfkc(converted);
+	}
+
+	std::string nfkc(const std::string& text)
+	{
+		int32_t errors = 0;
+
+		size_t length = utf8normalize(text.c_str(), text.size(), nullptr, 0, UTF8_NORMALIZE_COMPOSE | UTF8_NORMALIZE_COMPATIBILITY, &errors);
+		if (length == 0 ||
+			errors != 0)
+		{
+			return "";
+		}
+
+		char* buffer = new char[length + 1];
+		utf8normalize(text.c_str(), text.size(), buffer, length, UTF8_NORMALIZE_COMPOSE | UTF8_NORMALIZE_COMPATIBILITY, &errors);
+		buffer[length] = 0;
+
+		std::string converted = buffer;
+
+		delete [] buffer;
+
+		return converted;
+	}
+
+	std::string nfkd(unicode_t codepoint)
+	{
+		return nfkd(utf8(codepoint));
+	}
+
+	std::string nfkd(const std::string& text)
+	{
+		int32_t errors = 0;
+
+		size_t length = utf8normalize(text.c_str(), text.size(), nullptr, 0, UTF8_NORMALIZE_DECOMPOSE | UTF8_NORMALIZE_COMPATIBILITY, &errors);
+		if (length == 0 ||
+			errors != 0)
+		{
+			return "";
+		}
+
+		char* buffer = new char[length + 1];
+		utf8normalize(text.c_str(), text.size(), buffer, length, UTF8_NORMALIZE_DECOMPOSE | UTF8_NORMALIZE_COMPATIBILITY, &errors);
+		buffer[length] = 0;
+
+		std::string converted = buffer;
+
+		delete [] buffer;
+
+		return converted;
+	}
+
+	std::string PrintSequence(const std::string& text, uint8_t type)
+	{
+		std::stringstream ss;
+
+		if (text.length() == 0)
+		{
+			return ss.str();
+		}
+
+		std::stringstream ss_id;
+		ss_id << std::setfill(' ');
+		std::stringstream ss_ccc;
+		ss_ccc << std::setfill(' ');
+		std::stringstream ss_qc;
+		ss_qc << std::setfill(' ');
+
+		std::vector<unicode_t> codepoints = utf32(text);
+		for (std::vector<unicode_t>::iterator it = codepoints.begin(); it != codepoints.end(); ++it)
+		{
+			if (it != codepoints.begin())
+			{
+				ss_id << " ";
+				ss_ccc << " ";
+				ss_qc << " ";
+			}
+
+			std::string id = identifiable(*it);
+			std::string ccc = canonicalCombiningClass(*it);
+
+			size_t padding = std::max(id.length(), ccc.length());
+			if (type != 0)
+			{
+				std::string qc = quickCheck(*it, type);
+				padding = std::max(qc.length(), padding);
+				ss_qc << std::setw(padding) << qc;
+			}
+
+			ss_id << std::setw(padding) << id;
+			ss_ccc << std::setw(padding) << ccc;
+		}
+
+		ss << ss_id.str() << std::endl;
+		ss << "             " << ss_ccc.str();
+		if (type != 0)
+		{
+			ss << std::endl;
+			ss << "             " << ss_qc.str();
+		}
+
+		return ss.str();
+	}
+
+	::testing::AssertionResult CompareNormalizationCodepoint(
+		const char* expressionExpected, const char* expressionActual,
+		const NormalizationEntry& entryExpected, const NormalizationEntry& entryActual)
+	{
+		if (entryExpected.decomposed == entryActual.decomposed &&
+			entryExpected.composed == entryActual.composed &&
+			entryExpected.decomposedCompatibility == entryActual.decomposedCompatibility &&
+			entryExpected.composedCompatibility == entryActual.composedCompatibility)
+		{
+			return ::testing::AssertionSuccess();
+		}
+		else
+		{
+			::testing::AssertionResult result = ::testing::AssertionFailure();
+
+			result << entryExpected.name << " (" << identifiable(entryExpected.codepoint) << ")" << std::endl;
+
+			if (entryExpected.decomposed != entryActual.decomposed)
+			{
+				result << "[NFD] " << std::endl;
+				result << "    Actual:  " << PrintSequence(entryActual.decomposed, UnicodeProperty_Normalization_Decompose) << std::endl;
+				result << "  Expected:  " << PrintSequence(entryExpected.decomposed, UnicodeProperty_Normalization_Decompose) << std::endl;
+			}
+			else
+			{
+				result << "[NFD]        ";
+				result << PrintSequence(entryActual.decomposed, UnicodeProperty_Normalization_Decompose) << std::endl;
+			}
+
+			if (entryExpected.composed != entryActual.composed)
+			{
+				result << "[NFC] " << std::endl;
+				result << "    Actual:  " << PrintSequence(entryActual.composed, UnicodeProperty_Normalization_Compose) << std::endl;
+				result << "  Expected:  " << PrintSequence(entryExpected.composed, UnicodeProperty_Normalization_Compose) << std::endl;
+			}
+			else
+			{
+				result << "[NFC]        ";
+				result << PrintSequence(entryActual.composed, UnicodeProperty_Normalization_Compose) << std::endl;
+			}
+
+			if (entryExpected.decomposedCompatibility != entryActual.decomposedCompatibility)
+			{
+				result << "[NFKD]" << std::endl;
+				result << "    Actual:  " << PrintSequence(entryActual.decomposedCompatibility, UnicodeProperty_Normalization_Compatibility_Decompose) << std::endl;
+				result << "  Expected:  " << PrintSequence(entryExpected.decomposedCompatibility, UnicodeProperty_Normalization_Compatibility_Decompose) << std::endl;
+			}
+			else
+			{
+				result << "[NFKD]       ";
+				result << PrintSequence(entryActual.decomposedCompatibility, UnicodeProperty_Normalization_Compatibility_Decompose) << std::endl;
+			}
+
+			if (entryExpected.composedCompatibility != entryActual.composedCompatibility)
+			{
+				result << "[NFKC]" << std::endl;
+				result << "    Actual:  " << PrintSequence(entryActual.composedCompatibility, UnicodeProperty_Normalization_Compatibility_Compose) << std::endl;
+				result << "  Expected:  " << PrintSequence(entryExpected.composedCompatibility, UnicodeProperty_Normalization_Compatibility_Compose);
+			}
+			else
+			{
+				result << "[NFKC]       ";
+				result << PrintSequence(entryActual.composedCompatibility, UnicodeProperty_Normalization_Compatibility_Compose);
+			}
+
+			return result;
+		}
+	}
+
+	::testing::AssertionResult CompareNormalizationSequence(
+		const char* expressionExpected GTEST_ATTRIBUTE_UNUSED_, const char* expressionActual GTEST_ATTRIBUTE_UNUSED_,
+		const NormalizationEntry& entryExpected, const NormalizationEntry& entryActual)
+	{
+		if (entryExpected.decomposed == entryActual.decomposed &&
+			entryExpected.composed == entryActual.composed &&
+			entryExpected.decomposedCompatibility == entryActual.decomposedCompatibility &&
+			entryExpected.composedCompatibility == entryActual.composedCompatibility)
+		{
+			return ::testing::AssertionSuccess();
+		}
+		else
+		{
+			::testing::AssertionResult result = ::testing::AssertionFailure();
+
+			result << std::endl;
+
+			result << "[Source]     " << PrintSequence(entryExpected.sequence, 0) << std::endl;
+
+			if (entryExpected.decomposed != entryActual.decomposed)
+			{
+				result << "[NFD] " << std::endl;
+				result << "    Actual:  " << PrintSequence(entryActual.decomposed, UnicodeProperty_Normalization_Decompose) << std::endl;
+				result << "  Expected:  " << PrintSequence(entryExpected.decomposed, UnicodeProperty_Normalization_Decompose) << std::endl;
+			}
+			else
+			{
+				result << "[NFD]        ";
+				result << PrintSequence(entryActual.decomposed, UnicodeProperty_Normalization_Decompose) << std::endl;
+			}
+
+			if (entryExpected.composed != entryActual.composed)
+			{
+				result << "[NFC] " << std::endl;
+				result << "    Actual:  " << PrintSequence(entryActual.composed, UnicodeProperty_Normalization_Compose) << std::endl;
+				result << "  Expected:  " << PrintSequence(entryExpected.composed, UnicodeProperty_Normalization_Compose) << std::endl;
+			}
+			else
+			{
+				result << "[NFC]        ";
+				result << PrintSequence(entryActual.composed, UnicodeProperty_Normalization_Compose) << std::endl;
+			}
+
+			if (entryExpected.decomposedCompatibility != entryActual.decomposedCompatibility)
+			{
+				result << "[NFKD]" << std::endl;
+				result << "    Actual:  " << PrintSequence(entryActual.decomposedCompatibility, UnicodeProperty_Normalization_Compatibility_Decompose) << std::endl;
+				result << "  Expected:  " << PrintSequence(entryExpected.decomposedCompatibility, UnicodeProperty_Normalization_Compatibility_Decompose) << std::endl;
+			}
+			else
+			{
+				result << "[NFKD]       ";
+				result << PrintSequence(entryActual.decomposedCompatibility, UnicodeProperty_Normalization_Compatibility_Decompose) << std::endl;
+			}
+
+			if (entryExpected.composedCompatibility != entryActual.composedCompatibility)
+			{
+				result << "[NFKC]" << std::endl;
+				result << "    Actual:  " << PrintSequence(entryActual.composedCompatibility, UnicodeProperty_Normalization_Compatibility_Compose) << std::endl;
+				result << "  Expected:  " << PrintSequence(entryExpected.composedCompatibility, UnicodeProperty_Normalization_Compatibility_Compose);
+			}
+			else
+			{
+				result << "[NFKC]       ";
+				result << PrintSequence(entryActual.composedCompatibility, UnicodeProperty_Normalization_Compatibility_Compose);
+			}
+
+			return result;
+		}
+	}
+
+};
---
+++ source/tests/suite-utf8-normalize-decomposition.cpp
@@ -0,0 +1,262 @@
+#include "tests-base.hpp"
+
+#include "utf8rewind.h"
+
+#include "helpers-strings.hpp"
+
+TEST(NormalizeDecomposition, SingleBasicLatin)
+{
+	const char* i = "z";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+	int32_t errors = 0;
+
+	EXPECT_EQ(1, utf8normalize(i, is, o, os, UTF8_NORMALIZE_DECOMPOSE, &errors));
+	EXPECT_UTF8EQ("z", o);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(NormalizeDecomposition, SingleUnaffected)
+{
+	const char* i = "\xC2\xB5";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+	int32_t errors = 0;
+
+	EXPECT_EQ(2, utf8normalize(i, is, o, os, UTF8_NORMALIZE_DECOMPOSE, &errors));
+	EXPECT_UTF8EQ("\xC2\xB5", o);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(NormalizeDecomposition, SingleDecompose)
+{
+	const char* i = "\xE0\xA5\x98";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+	int32_t errors = 0;
+
+	EXPECT_EQ(6, utf8normalize(i, is, o, os, UTF8_NORMALIZE_DECOMPOSE, &errors));
+	EXPECT_UTF8EQ("\xE0\xA4\x95\xE0\xA4\xBC", o);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(NormalizeDecomposition, SingleHangulUnaffected)
+{
+	const char* i = "\xE1\x86\xBD";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+	int32_t errors = 0;
+
+	EXPECT_EQ(3, utf8normalize(i, is, o, os, UTF8_NORMALIZE_DECOMPOSE, &errors));
+	EXPECT_UTF8EQ("\xE1\x86\xBD", o);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(NormalizeDecomposition, SingleInvalidCodepoint)
+{
+	const char* i = "\xF4";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+	int32_t errors = 0;
+
+	EXPECT_EQ(3, utf8normalize(i, is, o, os, UTF8_NORMALIZE_DECOMPOSE, &errors));
+	EXPECT_UTF8EQ("\xEF\xBF\xBD", o);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(NormalizeDecomposition, SingleHangulDecomposeTwoCodepoints)
+{
+	const char* i = "\xEA\xB1\xB0";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+	int32_t errors = 0;
+
+	EXPECT_EQ(6, utf8normalize(i, is, o, os, UTF8_NORMALIZE_DECOMPOSE, &errors));
+	EXPECT_UTF8EQ("\xE1\x84\x80\xE1\x85\xA5", o);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(NormalizeDecomposition, SingleHangulDecomposeThreeCodepoints)
+{
+	const char* i = "\xEA\xB6\x83";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+	int32_t errors = 0;
+
+	EXPECT_EQ(9, utf8normalize(i, is, o, os, UTF8_NORMALIZE_DECOMPOSE, &errors));
+	EXPECT_UTF8EQ("\xE1\x84\x80\xE1\x85\xAE\xE1\x86\xBE", o);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(NormalizeDecomposition, SingleSequenceOrdered)
+{
+	const char* i = "\xC4\x88\xCC\x81";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+	int32_t errors = 0;
+
+	EXPECT_EQ(5, utf8normalize(i, is, o, os, UTF8_NORMALIZE_DECOMPOSE, &errors));
+	EXPECT_UTF8EQ("C\xCC\x82\xCC\x81", o);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(NormalizeDecomposition, SingleSequenceUnordered)
+{
+	const char* i = "A\xCC\x84\xCC\x9D";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+	int32_t errors = 0;
+
+	EXPECT_EQ(5, utf8normalize(i, is, o, os, UTF8_NORMALIZE_DECOMPOSE, &errors));
+	EXPECT_UTF8EQ("A\xCC\x9D\xCC\x84", o);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(NormalizeDecomposition, MultipleBasicLatin)
+{
+	const char* i = "Home";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+	int32_t errors = 0;
+
+	EXPECT_EQ(4, utf8normalize(i, is, o, os, UTF8_NORMALIZE_DECOMPOSE, &errors));
+	EXPECT_UTF8EQ("Home", o);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(NormalizeDecomposition, MultipleUnaffected)
+{
+	const char* i = "\xE1\xAA\xA8\xE1\xAA\x80\xE1\xAA\x87";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+	int32_t errors = 0;
+
+	EXPECT_EQ(9, utf8normalize(i, is, o, os, UTF8_NORMALIZE_DECOMPOSE, &errors));
+	EXPECT_UTF8EQ("\xE1\xAA\xA8\xE1\xAA\x80\xE1\xAA\x87", o);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(NormalizeDecomposition, MultipleDecompose)
+{
+	const char* i = "\xC5\x84\xC8\x8B\xC7\xBA";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+	int32_t errors = 0;
+
+	EXPECT_EQ(11, utf8normalize(i, is, o, os, UTF8_NORMALIZE_DECOMPOSE, &errors));
+	EXPECT_UTF8EQ("n\xCC\x81" "i\xCC\x91" "A\xCC\x8A\xCC\x81", o);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(NormalizeDecomposition, MultipleNonStarter)
+{
+	const char* i = "\xCC\x97\xCC\x9B\xCC\x80";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+	int32_t errors = 0;
+
+	EXPECT_EQ(6, utf8normalize(i, is, o, os, UTF8_NORMALIZE_DECOMPOSE, &errors));
+	EXPECT_UTF8EQ("\xCC\x9B\xCC\x97\xCC\x80", o);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(NormalizeDecomposition, MultipleInvalidCodepoints)
+{
+	const char* i = "\xEA\xF4\xC8";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+	int32_t errors = 0;
+
+	EXPECT_EQ(9, utf8normalize(i, is, o, os, UTF8_NORMALIZE_DECOMPOSE, &errors));
+	EXPECT_UTF8EQ("\xEF\xBF\xBD\xEF\xBF\xBD\xEF\xBF\xBD", o);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(NormalizeDecomposition, MultipleSequenceOrdered)
+{
+	const char* i = "\xC8\x82\xCC\x95\xCE\x8E\xCC\x81";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+	int32_t errors = 0;
+
+	EXPECT_EQ(11, utf8normalize(i, is, o, os, UTF8_NORMALIZE_DECOMPOSE, &errors));
+	EXPECT_UTF8EQ("A\xCC\x91\xCC\x95\xCE\xA5\xCC\x81\xCC\x81", o);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(NormalizeDecomposition, MultipleSequenceUnordered)
+{
+	const char* i = "\xC3\x8A\xCD\x87\xC3\x83\xCD\x9C\xCD\x88\xC3\xAD\xCC\x9B";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+	int32_t errors = 0;
+
+	EXPECT_EQ(17, utf8normalize(i, is, o, os, UTF8_NORMALIZE_DECOMPOSE, &errors));
+	EXPECT_UTF8EQ("E\xCD\x87\xCC\x82" "A\xCD\x88\xCC\x83\xCD\x9C" "i\xCC\x9B\xCC\x81", o);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(NormalizeDecomposition, MultipleSequenceHangul)
+{
+	const char* i = "\xEA\xB3\xA0\xE1\x84\x80\xE1\x85\xA9\xE1\x86\xB0\xEA\xB3\xBC";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+	int32_t errors = 0;
+
+	EXPECT_EQ(21, utf8normalize(i, is, o, os, UTF8_NORMALIZE_DECOMPOSE, &errors));
+	EXPECT_UTF8EQ("\xE1\x84\x80\xE1\x85\xA9\xE1\x84\x80\xE1\x85\xA9\xE1\x86\xB0\xE1\x84\x80\xE1\x85\xAA", o);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(NormalizeDecomposition, AmountOfBytes)
+{
+	const char* i = "a\xD6\xAE\xDC\xB3\xCC\x80\xCC\x95" "b";
+	size_t is = strlen(i);
+	int32_t errors = 0;
+
+	EXPECT_EQ(10, utf8normalize(i, is, nullptr, 0, UTF8_NORMALIZE_DECOMPOSE, &errors));
+	EXPECT_EQ(0, errors);
+}
+
+TEST(NormalizeDecomposition, InvalidData)
+{
+	const char* i = nullptr;
+	size_t is = 7;
+	char o[256] = { 0 };
+	size_t os = 255;
+	int32_t errors = 0;
+
+	EXPECT_EQ(0, utf8normalize(i, is, o, os, UTF8_NORMALIZE_DECOMPOSE, &errors));
+	EXPECT_UTF8EQ("", o);
+	EXPECT_EQ(UTF8_ERR_INVALID_DATA, errors);
+}
+
+TEST(NormalizeDecomposition, NotEnoughSpace)
+{
+	const char* i = "a\xD6\xAE\xDC\xB3\xCC\x80\xCC\x95" "b";
+	size_t is = strlen(i);
+	char o[5] = { 0 };
+	size_t os = 4;
+	int32_t errors = 0;
+
+	EXPECT_EQ(3, utf8normalize(i, is, o, os, UTF8_NORMALIZE_DECOMPOSE, &errors));
+	EXPECT_UTF8EQ("a\xD6\xAE", o);
+	EXPECT_EQ(UTF8_ERR_NOT_ENOUGH_SPACE, errors);
+}
