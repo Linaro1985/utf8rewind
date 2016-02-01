0ba914e20a7bbfc5aada2eef85aaaade40d01639
Quinten Lansu <knight666@gmail.com>
2016-02-01 17:44:46 +0100

    Clean up examples text.

---
+++ source/performance/performance-main.cpp
@@ -0,0 +1,6 @@
+#include "performance-base.hpp"
+
+int main(int argc, char** argv)
+{
+	return PERF_RUN_ALL(argc, argv);
+}
---
+++ source/tests/tests-main.cpp
@@ -0,0 +1,18 @@
+#include "tests-base.hpp"
+
+int main(int argc, char** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+
+	int result = RUN_ALL_TESTS();
+
+	if (result != 0)
+	{
+		std::cout << std::endl;
+		std::cout << "Press any key to continue.";
+
+		std::cin.get();
+	}
+
+	return result;
+}
